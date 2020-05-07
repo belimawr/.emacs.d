55b2a35d56d06963bc6fca9d434d5a9ef99c1d7a
Tiago Queiroz <contato@tiago.eti.br>
2020-05-07 16:35:00 +0200

    Updating system configuration
    Also adding more base packages

--- scripts/configure-system.sh
+++ scripts/configure-system.sh
@@ -1,4 +1,9 @@
 #! /bin/sh
 
+mkdir -p /usr/local/share/kbd/keymaps/
+cp ../system-files/usr/local/share/kbd/keymaps/us-caps-2-ctrl.map /usr/local/share/kbd/keymaps/
+localectl set-keymap --no-convert us-caps-2-ctrl
+echo "KEYMAP=/usr/local/share/kbd/keymaps/us-caps-2-ctrl.map" >> /etc/vconsole.conf
+echo "LANG=en_GB.UTF-8" >> /etc/locale.conf
 cat ../system-files/etc/profile >>  /etc/profile
 cat ../system-files/etc/environment >> /etc/environment
