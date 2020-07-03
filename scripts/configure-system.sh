ef682c8e444b73a8f7105f3de053da283e450292
Tiago Queiroz <contato@tiago.eti.br>
2020-07-03 10:54:35 +0200

    Adding UK keyboard variant

--- scripts/configure-system.sh
+++ scripts/configure-system.sh
@@ -1,9 +1,11 @@
 #! /bin/sh
 
 mkdir -p /usr/local/share/kbd/keymaps/
-cp ../system-files/usr/local/share/kbd/keymaps/us-caps-2-ctrl.map /usr/local/share/kbd/keymaps/
+cp ../system-files/usr/local/share/kbd/keymaps/* /usr/local/share/kbd/keymaps/
 localectl set-keymap --no-convert us-caps-2-ctrl
+#localectl set-keymap --no-convert uk-caps-2-ctrl
 echo "KEYMAP=/usr/local/share/kbd/keymaps/us-caps-2-ctrl.map" >> /etc/vconsole.conf
+#echo "KEYMAP=/usr/local/share/kbd/keymaps/uk-caps-2-ctrl.map" >> /etc/vconsole.conf
 echo "LANG=en_GB.UTF-8" >> /etc/locale.conf
 cat ../system-files/etc/profile >>  /etc/profile
 cat ../system-files/etc/environment >> /etc/environment
